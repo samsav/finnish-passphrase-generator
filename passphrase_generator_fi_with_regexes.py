a8caf98edbc74036c9a735f56cef6f693d8e49a0
Sampo Savolainen <savolainen.sampo@gmail.com>
2018-03-11 11:31:19 +0200

    Clean up and comment code

--- passphrase_generator_fi_with_regexes.py
+++ passphrase_generator_fi_with_regexes.py
@@ -9,6 +9,7 @@ import re
 # - add support for alternative forms in e.g. plural genitive and partitive?
 # - fix vowel harmony
 # - enable clitics
+# - add support for forming compounds?
 
 
 def prepare_full_list(file_path):
@@ -31,7 +32,7 @@ def prepare_full_list(file_path):
 # these appear to be compound nouns. Currently these entries are excluded.
 def select_inflection_paradigms(word_entries, lower_limit, upper_limit):
     """Select word entries that have specific inflection paradigms
-       in the Kotus word list."""
+       in the Kotus word list. Return the word entries as a list."""
 
     print("Picking words with desired inflection paradigms...")
     word_entries_with_selected_infls = []
@@ -51,11 +52,10 @@ def pick_random_set(word_list, size_of_set):
 
 
 def compose_nouns(word_list):
-    """Prepend all noun entries in the list
-    with their inflection and gradation paradigms."""
+    """Prepend all noun entries in the list with their inflection
+       and gradation paradigms. Return the entries as a list."""
 
     print("Composing noun entries...")
-
     nouns = []
     for word_entry in word_list:
         # replace spaces with underscores to simplify later regexes
@@ -74,13 +74,13 @@ def compose_nouns(word_list):
     return nouns
 
 
-def attach_noun_endings(word):
-    """Attach random number, inflection, and clitic endings to a noun"""
+def attach_noun_endings(noun):
+    """Attach random number, inflection, and clitic endings to a noun."""
     number = random.choice(number_set)
     inflection = random.choice(inflection_set)
     # clitic = random.choice(clitic_set)
-    word = word + number + inflection
-    return word
+    noun = noun + number + inflection
+    return noun
 
 
 number_set = ['+Sg', '+Pl']
@@ -93,10 +93,9 @@ inflection_set = ['+Nom', '+Gen', '+Par',
 clitic_set = ['', 'hAn', 'kin', 'kO', 'pA']
 
 
-# This approach to handling the necessary regex functions using closures
-# to dynamically build the functions from regex patterns given as
-# parameters is taken from Dive into Python 3 by Mark Pilgrim
-# (see http://www.diveintopython3.net/generators.html)
+# This approach of handling the necessary regex functions by using closures
+# to dynamically build the functions is taken from Dive into Python 3
+# by Mark Pilgrim (see http://www.diveintopython3.net/generators.html)
 def build_inflect_functions(pattern, search, replace):
     """Dynamically build regex search and replace functions."""
 
@@ -116,11 +115,12 @@ def initialize_rules(pattern_file):
     rules = []
     with open(pattern_file, encoding='utf-8') as fp:
         for line in fp:
-            if line[0] in ['#', '\n', '\r']:
+            if line[0] in ['#', '\n', '\r']:  # skip comments and empty lines
                 continue
             pattern, search, replace = line.split(None, 3)
             rules.append(build_inflect_functions(
-                                        pattern, search, replace))
+                            pattern, search, replace))
+
     return rules
 
 
@@ -130,7 +130,7 @@ inflections = initialize_rules('inflection-patterns.txt')
 
 
 def gradate(word):
-    """Check if a word matches any gradation rules: if yes, return the word
+    """Check if a word matches any gradation patterns: if yes, return the word
        with gradation replace rules applied. If no, return the original word
        in order to avoid returning None values."""
     for match_rule, grad_rule in gradations:
@@ -141,7 +141,7 @@ def gradate(word):
 
 
 def inflect(word):
-    """Check if a word matches any inflection rules: if yes, return the word
+    """Check if a word matches any inflection patterns: if yes, return the word
        with inflection replace rules applied. If no, return the original word
        in order to avoid returning None values."""
     for match_rule, inflect_rule in inflections:
@@ -161,12 +161,13 @@ def convert_to_plural(word):
 
 def apply_consonant_gradation(word_list):
     """Apply consonant gradation rules to a list of words and return
-       a new list."""
+       the results in a list."""
     # include a print statement for debugging
     print("\nApplying gradation rules to these lexical forms:")
     gradated_words = []
     for word in word_list:
         print(word)
+        # TODO: move checking for plurals to a separate function
         if re.search(r't\+Sg', word):
             word = convert_to_plural(word)
         word = gradate(word)
@@ -176,7 +177,7 @@ def apply_consonant_gradation(word_list):
 
 def apply_inflection_rules(word_list):
     """Apply inflection rules to a list of words and return
-       a new list."""
+       the results in a list."""
     # include a print statement for debugging
     print("\nApplying inflection rules to these lexical forms:")
     inflected_words = []
@@ -192,7 +193,7 @@ def apply_inflection_rules(word_list):
 
 def apply_vowel_harmony(word_list):
     """Apply vowel harmony transformations to a list of words
-       and return a new list."""
+       and return the results in a list."""
     # include a print statement for debugging
     print("\nApplying vowel harmony to these forms:")
     words_with_vowel_harmony = []
@@ -208,36 +209,81 @@ def apply_vowel_harmony(word_list):
 
 
 def form_passphrase(word_list):
-    """A helper function for turning random words in a list into
-       a passphrase that is returned as a string."""
+    """A helper function for turning four random words in a list
+       into a passphrase that is returned as a string."""
     four_words = pick_random_set(word_list, 4)
     phrase = ' '.join(four_words)
     return phrase
 
 
+def debug():
+    """Printouts for debugging consonant gradation patterns
+       and other features"""
+
+    print("Debugging consonant gradation:")
+    print("\nSingulars:")
+    singulars = apply_consonant_gradation(['<N1A>baarimikko+Sg+Tra',
+                                           '<N5C>attentaatti+Sg+Tra',
+                                           '<N5B>kaappi+Sg+Tra',
+                                           '<N9E>lapa+Sg+Gen',
+                                           '<N48E>taive+Sg+Gen',
+                                           '<N1F>satu+Sg+All',
+                                           '<N5J>evakuointi+Sg+Gen'])
+    print("\nApplied consonant gradation:")
+    for word in singulars:
+        print(word)
+
+    print("\nPlurals:")
+    plurals = apply_consonant_gradation(['<N1A>baarimikko+Pl+Tra',
+                                         '<N5C>attentaatti+Pl+Tra',
+                                         '<N5B>kaappi+Pl+Tra',
+                                         '<N9E>lapa+Pl+Nom',
+                                         '<N48E>taive+Pl+Nom',
+                                         '<N1F>satu+Pl+Ine',
+                                         '<N5J>evakuointi+Pl+Ade',
+                                         '<N5J>fosforointi+Pl+Gen'])
+    print("\nApplied consonant gradation:")
+    for word in plurals:
+        print(word)
+
+    print("\nDebugging word-internal hyphens:")
+    words = apply_inflection_rules(apply_consonant_gradation(
+            ['<N6>agar-agar+Pl+Abl']
+            ))
+    for word in words:
+        print(word)
+
+    print("\nDebugging plural-only words:")
+    words = apply_inflection_rules(apply_consonant_gradation(
+            ['<N1>aivot+Sg+Abl']
+            ))
+    for word in words:
+        print(word)
+
+
 def main():
     print("Welcome to the Finnish passphrase generator!\n")
 
+    # Parse the XML file for the full word list
     full_word_list = prepare_full_list('kotus_sanalista/testilista.xml')
 
     # Pick nouns from inflection paradigms 1-6
     nouns = select_inflection_paradigms(full_word_list, 1, 6)
 
     while True:
-
-        komento = input("Generate phrases? y/n ")
-
-        if komento == "n":
+        # Simple control loop that only enforces exit condition
+        command = input("Generate phrases? y/n ")
+        if command == "n":
             break
 
+        # For each run of the loop, pick a new random set from the list of
+        # nouns created earlier. Then determine their inflection and gradation
+        # paradigms using the compose_nouns() function and apply gradation,
+        # inflection, and vowel harmony rules in sequence.
         random_nouns = pick_random_set(nouns, 50)
-
         lexical_nouns = compose_nouns(random_nouns)
-
         gradated_nouns = apply_consonant_gradation(lexical_nouns)
-
         inflected_nouns = apply_inflection_rules(gradated_nouns)
-
         final_nouns = apply_vowel_harmony(inflected_nouns)
 
         print("\nHere are the words with all transformations applied:")
@@ -245,63 +291,11 @@ def main():
             print(word)
 
         print("\nHere are some possible phrases:\n")
-
         for i in range(0, 4):
             phrase = form_passphrase(final_nouns)
             print("{0} merkkiä: {1}\n".format(len(phrase), phrase))
 
         print()
-
-        print("Debugging consonant gradation:\n")
-        print("\nSingulars:")
-        singulars = apply_consonant_gradation(['<N1A>baarimikko+Sg+Tra',
-                                               '<N5C>attentaatti+Sg+Tra',
-                                               '<N5B>kaappi+Sg+Tra',
-                                               '<N9E>lapa+Sg+Gen',
-                                               '<N48E>taive+Sg+Gen',
-                                               '<N1F>satu+Sg+All',
-                                               '<N5J>evakuointi+Sg+Gen'])
-
-        print("\nApplied consonant gradation:")
-        for word in singulars:
-            print(word)
-
-        print()
-
-        print("Plurals:")
-        plurals = apply_consonant_gradation(['<N1A>baarimikko+Pl+Tra',
-                                             '<N5C>attentaatti+Pl+Tra',
-                                             '<N5B>kaappi+Pl+Tra',
-                                             '<N9E>lapa+Pl+Nom',
-                                             '<N48E>taive+Pl+Nom',
-                                             '<N1F>satu+Pl+Ine',
-                                             '<N5J>evakuointi+Pl+Ade',
-                                             '<N5J>fosforointi+Pl+Gen'])
-
-        print("\nApplied consonant gradation:")
-        for word in plurals:
-            print(word)
-
-        print()
-
-        print("Debugging word-internal hyphens:")
-
-        words = apply_inflection_rules(apply_consonant_gradation(
-                ['<N6>agar-agar+Pl+Abl']
-                ))
-
-        for word in words:
-            print(word)
-
-        print()
-
-        print("Debugging plural-only words:")
-
-        words = apply_inflection_rules(apply_consonant_gradation(
-                ['<N1>aivot+Sg+Abl']
-                ))
-        for word in words:
-            print(word)
 
 
 if __name__ == '__main__':
