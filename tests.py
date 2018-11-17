cae2f989b9602f04f437de4bc4c3691db03a9a42
Sampo Savolainen <savolainen.sampo@gmail.com>
2018-11-17 12:52:48 +0200

    Update gradation pattern F to handle compounds päätekohta -> päätekohdan

--- tests.py
+++ tests.py
@@ -13,7 +13,8 @@ def debug():
         '<N1A>baarimikko+Sg+Tra', '<N48A>hake+Sg+Gen',
         '<N5C>attentaatti+Sg+Tra', '<N5B>kaappi+Sg+Tra', '<N9E>lapa+Sg+Gen',
         '<N48E>taive+Sg+Gen', '<N1F>satu+Sg+All', '<N10F>risteyskohta+Sg+Abl',
-        '<N5J>evakuointi+Sg+Gen', '<N9D>vika+Sg+Gen', '<N32D>ien+Sg+Ill'
+        '<N10F>päätekohta+Sg+Ela', '<N5J>evakuointi+Sg+Gen', '<N9D>vika+Sg+Gen',
+        '<N32D>ien+Sg+Ill'
     ])
     print("\nApplied consonant gradation:")
     for word in singulars:
