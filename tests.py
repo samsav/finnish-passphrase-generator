9c864a2188edfd3c97864d079158a6ed9331ffe0
Sampo Savolainen <savolainen.sampo@gmail.com>
2018-11-17 12:00:38 +0200

    Update imports

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 """Tests for the passphrase generator"""
 
-import passphrase_generator_fi as ppgen
+import nlp
 
 
 def debug():
@@ -9,7 +9,7 @@ def debug():
 
     print("Debugging consonant gradation:")
     print("\nSingulars:")
-    singulars = ppgen.apply_consonant_gradation([
+    singulars = nlp.apply_consonant_gradation([
         '<N1A>baarimikko+Sg+Tra', '<N48A>hake+Sg+Gen',
         '<N5C>attentaatti+Sg+Tra', '<N5B>kaappi+Sg+Tra', '<N9E>lapa+Sg+Gen',
         '<N48E>taive+Sg+Gen', '<N1F>satu+Sg+All', '<N5J>evakuointi+Sg+Gen',
@@ -20,7 +20,7 @@ def debug():
         print(word)
 
     print("\nPlurals:")
-    plurals = ppgen.apply_consonant_gradation([
+    plurals = nlp.apply_consonant_gradation([
         '<N1A>baarimikko+Pl+Tra', '<N5C>attentaatti+Pl+Tra',
         '<N5B>kaappi+Pl+Tra', '<N9E>lapa+Pl+Nom', '<N48E>taive+Pl+Nom',
         '<N1F>satu+Pl+Ine', '<N5J>evakuointi+Pl+Ade',
@@ -31,31 +31,31 @@ def debug():
         print(word)
 
     print("\nDebugging word-internal hyphens:")
-    words = ppgen.apply_inflection_rules(
-        ppgen.apply_consonant_gradation(['<N6>agar-agar+Pl+Abl']))
+    words = nlp.apply_inflection_rules(
+        nlp.apply_consonant_gradation(['<N6>agar-agar+Pl+Abl']))
     for word in words:
         print(word)
 
     print("\nDebugging plural-only words:")
-    words = ppgen.apply_inflection_rules(
-        ppgen.apply_consonant_gradation(['<N1>aivot+Sg+Abl']))
+    words = nlp.apply_inflection_rules(
+        nlp.apply_consonant_gradation(['<N1>aivot+Sg+Abl']))
     for word in words:
         print(word)
 
     print("\nDebugging loan words ending in consonants:")
-    words = ppgen.apply_inflection_rules(
-        ppgen.apply_consonant_gradation(['<N5>ekstranet+Pl+Tra']))
+    words = nlp.apply_inflection_rules(
+        nlp.apply_consonant_gradation(['<N5>ekstranet+Pl+Tra']))
     for word in words:
         print(word)
 
     print("\nDebugging inflection patterns:")
-    words = ppgen.apply_inflection_rules(
-        ppgen.apply_consonant_gradation([
+    words = nlp.apply_inflection_rules(
+        nlp.apply_consonant_gradation([
             '<N7>salmi+Pl+Tra', '<N7>salmi+Sg+Ill', '<N8>genre+Sg+Ill',
             '<N8>genre+Pl+Ill', '<N9J>kähmintä+Pl+Par', '<N9>kala+Pl+Ill',
             '<N10F>pöytä+Sg+Gen', '<N10>koira+Pl+Ill'
         ]))
-    for word in ppgen.apply_vowel_harmony(words):
+    for word in nlp.apply_vowel_harmony(words):
         print(word)
     print()
 
