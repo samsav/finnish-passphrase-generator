a2eceb0d353f9bec2de8c0d0f20316c55e8776e6
Sampo Savolainen <sampo.savolainen@proton.me>
2024-02-14 13:30:50 -0800

    Use new CSV word list

--- fin_ppgen/generator.py
+++ fin_ppgen/generator.py
@@ -1,8 +1,10 @@
 """A simple passphrase generator for Finnish"""
 
+from pathlib import Path
 import os
 import secrets
-from bs4 import BeautifulSoup
+import csv
+import re
 import nlp
 
 
@@ -16,18 +18,37 @@ import nlp
 # - move all print statements to main function
 
 
-def read_kotus_xml(file_path):
-    """Parse an XML file containing a word list with BeautifulSoup and return
-       a bs4.element.ResultSet object with all word entries from the file."""
+def read_csv_to_dict(csv_file_path: Path) -> dict[str, dict[str, list[str]]]:
+    """
+    Reads a CSV file containing word data and parses it into a dictionary.
 
-    print("Parsing XML file...\n")
-    with open(file_path, 'r') as fp:
-        soup = BeautifulSoup(fp, 'lxml')
-    # Read all word entries directly into a variable: the resulting
-    # bs4.element.ResultSet object can be manipulated like a list
-    print("Listing word entries...\n")
-    word_entries = soup.find_all('st')
-    return word_entries
+    Args:
+        csv_file_path (Path): The path to the CSV file.
+
+    Returns:
+        dict[str, dict[str, list[str]]]: A dictionary where keys are words
+        and values are dictionaries containing word data about its word class and
+        inflection paradigms. The values may be strings or lists of strings.
+    """
+    word_dict: dict[str, dict[str, str | list[str]]] = {}
+    with open(csv_file_path, 'r', newline='', encoding='utf-8') as file:
+        csv_reader = csv.DictReader(file, delimiter='\t')
+        for row in csv_reader:
+            # The word itself is in the 'Hakusana' column.
+            word: str = row['Hakusana']
+            # The word class is in the 'Sanaluokka' column.
+            # Some words can belong to more than one word class.
+            word_class_list: list[str] = [wc.strip() for wc in row['Sanaluokka'].split(',')]
+            # Some words have alternative inflection paradigms separated by a comma.
+            # Remove parentheses that denote optionality for a paradigm and strip any whitespace.
+            # Words that do not have inflection data associated with them have an empty string in the 'Taivutustiedot' column.
+            inflection_paradigm_list: list[str] = [re.sub(r'[\(\)]', '', p.strip() if p else 'no_inflection_data') for p in row['Taivutustiedot'].split(',')]
+            
+            word_dict[word] = {
+                'word_class': word_class_list,
+                'inflection_paradigm': inflection_paradigm_list,
+            }
+    return word_dict
 
 
 # TODO: use *args to pass an arbitrary set of inflection numbers
@@ -36,13 +57,15 @@ def read_kotus_xml(file_path):
 # these appear to be compound nouns. Currently these entries are excluded.
 def select_inflection_paradigms(word_entries, lower_limit, upper_limit):
     """Select word entries that have specified inflection paradigms
-       in the Kotus word list. Return the word entries as a list."""
+    in the Kotus word list. Return the word entries as a list."""
 
-    print("Picking words with desired inflection paradigms...")
     word_entries_with_selected_infls = []
     for entry in word_entries:
-        if entry.t is not None and int(entry.t.tn.string) >= lower_limit \
-           and int(entry.t.tn.string) <= upper_limit:
+        if (
+            entry.t is not None
+            and int(entry.t.tn.string) >= lower_limit
+            and int(entry.t.tn.string) <= upper_limit
+        ):
             word_entries_with_selected_infls.append(entry)
     return word_entries_with_selected_infls
 
@@ -55,22 +78,22 @@ def random_set(word_list, size_of_set):
 
 def form_passphrase(wordlist, length):
     """A helper function for turning random words in a list
-       into a passphrase that is returned as a string."""
+    into a passphrase that is returned as a string."""
     words = random_set(wordlist, length)
     # clear possible internal spaces from words
-    return ' '.join([word.replace(' ', '') for word in words])
+    return " ".join([word.replace(" ", "") for word in words])
 
 
 def kaikkikotona(nouns):
     nouns = nlp.prepend_lexical_info(nouns)
-    nouns[0] = nouns[0] + '+Pl' + '+Nom'
-    nouns[1] = nouns[1] + '+Sg' + '+Ine'
+    nouns[0] = nouns[0] + "+Pl" + "+Nom"
+    nouns[1] = nouns[1] + "+Sg" + "+Ine"
     nouns = nlp.apply_consonant_gradation(nouns)
     nouns = nlp.apply_inflection_rules(nouns)
     nouns = nlp.apply_other_transformations(nouns)
     nouns = nlp.apply_vowel_harmony(nouns)
 
-    return f'Ei ole kaikki {nouns[0]} {nouns[1]}'
+    return f"Ei ole kaikki {nouns[0]} {nouns[1]}"
 
 
 def main():
@@ -78,9 +101,11 @@ def main():
 
     print("Welcome to the Finnish passphrase generator!\n")
 
-    data_path = os.path.abspath('../lang_data/kotus-sanalista_v1.xml')
-    # Parse the XML file for the full word list
-    full_word_list = read_kotus_xml(data_path)
+    csv_file_path: os.PathLike = Path(
+        "../lang_data/nykysuomensanalista2022.csv"
+    ).resolve()
+    # Read the CSV word list file into a dict
+    word_dict = read_csv_to_dict(csv_file_path)
 
     # Pick nouns from inflection paradigms 1-15
     nouns = select_inflection_paradigms(full_word_list, 1, 15)
@@ -105,14 +130,17 @@ def main():
         almost_done = nlp.apply_other_transformations(inflected_nouns)
         final_nouns = nlp.apply_vowel_harmony(almost_done)
 
-        processing_chain = [[
-            lexical_nouns[i], gradated_nouns[i], inflected_nouns[i],
-            final_nouns[i]
-        ] for i in range(50)]
+        processing_chain = [
+            [lexical_nouns[i], gradated_nouns[i], inflected_nouns[i], final_nouns[i]]
+            for i in range(50)
+        ]
 
         print("\nHere are the transformations applied to the words:")
-        print("\n{:30}\t{:30}\t{:20}\t{:20}\t".format("LEXICAL", "GRADATED",
-                                                      "INFLECTED", "FINAL"))
+        print(
+            "\n{:30}\t{:30}\t{:20}\t{:20}\t".format(
+                "LEXICAL", "GRADATED", "INFLECTED", "FINAL"
+            )
+        )
         for word in processing_chain:
             print("{:30}\t{:30}\t{:20}\t{:20}\t".format(*word))
 
@@ -121,11 +149,6 @@ def main():
             phrase = form_passphrase(final_nouns, 4)
             print("{0} characters: {1}\n".format(len(phrase), phrase))
 
-        # print()
 
-        # kaikki_nouns = random_set(nouns, 2)
-        # print(kaikkikotona(kaikki_nouns))
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
